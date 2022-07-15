3cb8c323bc17a20f18e416ce4f9b9805e5949fcb
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-07-15 10:37:41 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import './App.css';
+import randomWords from 'random-words';
+import { Refresh } from 'tabler-icons-react';
+
+const videos = [
+  'D7vl1XHfRcc',
+  'yVpVtBn5egg',
+  'jwq3Uz3H1y8',
+  'D_JxqZgUS7E',
+  'xqUz76TJ9ZA',
+  'LXmg-JFXbMA',
+  'TIx-HFBAjhY',
+  'wGXBceRwivg',
+  'SU14ZpCl7X8',
+  'vIcM2U-Nj9o',
+  'JB0wJ9n0NRQ',
+  'P4CYUruI7J4',
+  '2gZ5UPk6RpU',
+  'QeSOs9jJHxM',
+];
+
+const randomVideo = videos[Math.floor(Math.random() * videos.length)];
+
+function App() {
+  const [words, setWords] = useState<string>(
+    randomWords({ exactly: 3, join: ' ' })
+  );
+
+  function newWords() {
+    setWords(randomWords({ exactly: 3, join: ' ' }));
+  }
+
+  return (
+    <div className='flex flex-col gap-4 w-screen h-screen justify-center items-center'>
+      <h1 className='text-2xl font-serif font-bold'>anythreewords</h1>
+      <div className='flex flex-col bg-cyan-300 w-80 max-w-full p-1'>
+        <div className='flex justify-between items-center'>
+          <p className='font-serif text-xs m-0'>your three words:</p>
+          <Refresh size={14} onClick={newWords} />
+        </div>
+        <div className='m-3 font-serif font-bold'>{words}</div>
+      </div>
+      <div className='video-responsive flex flex-col items-center w-100'>
+        <iframe
+          width='300'
+          height='200'
+          src={`https://www.youtube.com/embed/${randomVideo}`}
+          frameBorder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+          title='Embedded youtube'
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
